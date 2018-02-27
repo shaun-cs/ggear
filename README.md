e8551d48e924924651ed7af222f32a928065b0e3
shaun-cs <sbaker@easynet.co.uk>
2018-02-27 09:40:34 +0100

    first working hello-world type application

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ggear
+
+## setup
+1. npm init -y
+2. npm install
+
+## operation
+test: `npm test`
+
+start using live-server: `npm start`
+
+## tricky things
+All packages upgraded ok, except for **SystemJS**.
+This package has changed radically, and is not backward compatible.
+It now needs a plugin called **plugin-typescript** in order to be able to find and load the **typescript** package.
+The documentation for configuring **SystemJS** is sketchy; what I have done in **systemjs.config.js** is more trial and error than certainty.
+Would be loathed to deploy this into production without fully understanding it.
+
+## TODOs
+
+##### SystemJS configuration: 
+really understand what configuration files are needed and how it works with typescript
+
---
+++ systemjs.config.js
@@ -0,0 +1,24 @@
+System.config({
+    transpiler: 'ts',
+    typescriptOptions: {experimentalDecorators: true, emitDecoratorMetadata: true},
+    map: {
+      '@angular': 'node_modules/@angular',
+      'rxjs': 'node_modules/rxjs',
+      'ts': 'node_modules/plugin-typescript',
+      'typescript': 'node_modules/typescript'
+    },
+    meta: {
+      '@angular/*': {'format': 'cjs'}
+    },
+    packages: {
+      'app': {main: 'main', defaultExtension: 'ts'},
+      'rxjs': {main: 'Rx'},
+      '@angular/core': {main: 'bundles/core.umd.min.js'},
+      '@angular/common': {main: 'bundles/common.umd.min.js'},
+      '@angular/compiler': {main: 'bundles/compiler.umd.min.js'},
+      '@angular/platform-browser': {main: 'bundles/platform-browser.umd.min.js'},
+      '@angular/platform-browser-dynamic': {main: 'bundles/platform-browser-dynamic.umd.min.js'},
+      'ts': {main: 'lib/plugin.js'},
+      'typescript': {main: 'lib/typescript.js',meta: {'lib/typescript.js': {'exports': 'ts'}}}
+    }
+});
