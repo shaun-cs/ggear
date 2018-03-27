496de0f86607913c283f22df8cd85d76f48ec77c
shaun-cs <sbaker@easynet.co.uk>
2018-03-28 00:04:56 +0200

    added routing to support home page and product details in single page

--- app/components/product-item/product-item.ts
+++ app/components/product-item/product-item.ts
@@ -3,6 +3,7 @@ import {Product} from '../../services/product-service';
 
 @Component({
     selector: 'auction-product-item',
+    styleUrls: ['app/components/product-item/product-item.css'],
     templateUrl: 'app/components/product-item/product-item.html'
 })
 export default class ProductItemComponent {
--- systemjs.config.js
+++ systemjs.config.js
@@ -24,6 +24,7 @@ System.config({
       '@angular/core': {main: 'bundles/core.umd.min.js'},
       '@angular/common': {main: 'bundles/common.umd.min.js'},
       '@angular/compiler': {main: 'bundles/compiler.umd.min.js'},
+      '@angular/router': {main: 'bundles/router.umd.min.js'},
       '@angular/platform-browser': {main: 'bundles/platform-browser.umd.min.js'},
       '@angular/platform-browser-dynamic': {main: 'bundles/platform-browser-dynamic.umd.min.js'},
       'ts': {main: 'lib/plugin.js'},
